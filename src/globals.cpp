ab88b42eb78962e04a21db5c7dc90f18da04c672
Aaron Jones <aaron11193@gmail.com>
2021-12-15 15:24:59 +1000

    update

--- src/globals.cpp
+++ src/globals.cpp
@@ -3,7 +3,7 @@
 int activePIDs[96];
 std::bitset<96> enabledPids = {0};
 int numPids = 0;
-int currentPIDValues[20] = {0};
+int currentPIDValues[96] = {0};
 
 unsigned long int in1Debounce = 0;
 unsigned long int in2Debounce = 0;
